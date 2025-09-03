579c1fc52f579a7a3897e3762e65753e67610b9e
Kulasq <lucasdantas_11@hotmail.com>
2025-09-03 14:52:39 -0300

    adjusting test_product_viewset to accept tolken

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -2,6 +2,7 @@ import json
 
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
 
 from django.urls import reverse
 
@@ -15,6 +16,8 @@ class TestProductViewSet(APITestCase):
 
     def setUp(self):
         self.user = UserFactory()
+        token = Token.objects.create(user=self.user)
+        token.save()
 
         self.product = ProductFactory(
             title='pro controler',
@@ -22,6 +25,8 @@ class TestProductViewSet(APITestCase):
         )
 
     def test_get_all_products(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         response = self.client.get(
             reverse('product-list', kwargs={'version': 'v1'})
         )
@@ -34,6 +39,8 @@ class TestProductViewSet(APITestCase):
         self.assertEqual(product_data['results'][0]['active'], self.product.active)
 
     def test_create_product(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         category = CategoryFactory()
         data = json.dumps({
             'title': 'notebook',
